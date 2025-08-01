8e74c3e09eb0adcce2a090b651319141e5956383
Finn <1050731112@qq.com>
2025-08-02 03:40:27 +0800

    using settings.json to ignore cpp syntax check for a specific folder

---
+++ 2. Data Structures & Algorithms/C & C++/lc-plugin-template/cpp-template/leetcode/editor/cn/rearranging-fruits.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=2561 lang=cpp
+ * @lcpr version=30202
+ *
+ * [2561] 重排水果
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// [4,2,2,2]\n[1,4,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,3,4,1]\n[3,2,5,1]\n
+// @lcpr case=end
+
+ */
+
