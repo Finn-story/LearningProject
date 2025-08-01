49bf436e9e62a217b935d4b824c1489c6495fedd
Finn <1050731112@qq.com>
2025-08-01 05:55:28 +0100

    add extension to practice c++ in leetcode

---
+++ 2. Data Structures & Algorithms/C & C++/lc-plugin-template/cpp-template/leetcode/editor/cn/pascals-triangle.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=cpp
+ * @lcpr version=30202
+ *
+ * [118] 杨辉三角
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// 5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+ */
+
